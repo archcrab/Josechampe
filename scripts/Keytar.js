00d4fa32d8d3d1328c35976656b48c68b2f5b02e
archcrab <ianzhengwenn620@163.com>
2021-05-08 15:23:13 +0800

    Rendering

--- scripts/Keytar.js
+++ scripts/Keytar.js
@@ -1,31 +1,120 @@
 function KeytarMain(){
 	
 	// Loaded Song 
-	var qNotes = [];
-	var wNotes = [];
-	var eNotes = [];
-	var rNotes = [];
-	var tNotes = [];
+	this.qNotes = [];
+	this.wNotes = [];
+	this.eNotes = [];
+	this.rNotes = [];
+	this.tNotes = [];
 	
 	// Game Parameters 
-	var points; 
-	var multiplier; 
-	var rockLevel; 
+	this.points = 0; 
+	this.multiplier = 1; 
+	this.rockLevel = 50; 
+	
+	this.keys = [];
 }
 
-function Note(){
-	this.color;
-	this.img;
-	this.x;
-	this.y;
+KeytarMain.prototype.loadSong = function(song){
+	this.qNotes = song[0];
+	this.wNotes = song[1];
+	this.eNotes = song[2];
+	this.rNotes = song[3];
+	this.tNotes = song[4];
+}
+
+function GreenNote(){
+	this.img = new Image();
+	this.x = 100;
+	this.y = 100;
+	
+	console.log(this.img);
+	
+	this.img.src = "images/doge.jpg";
 }
 
 function NoteReader(x,y){
 	this.x = x;
 	this.y = y;
+	this.img;
 }
 
-document.onkeydown = function (e) {
+NoteReader.prototype.checkHit = function(y) {
+	//return y < this.y + 50 && y > this.y - 50;
 	
+	if(kMain.keys.includes(81) && kR.qNotes[0].y < this.y + 50 && kR.qNotes[0].y > this.y - 50 ){
+		kMain.score += 50 * multiplier;
+		kMain.rockLevel += 1;
+		kR.qNotes.shift();
+	}
+	else{
+		kMain.rockLevel -= 2;
+	}
+	
+	if(kMain.keys.includes(87) && kR.wNotes[0].y < this.y + 50 && kR.wNotes[0].y > this.y - 50 ){
+		kMain.score += 50 * multiplier;
+		kMain.rockLevel += 1;
+		kR.wNotes.shift();
+	}
+	else{
+		kMain.rockLevel -= 2;
+	}
+	
+	if(kMain.keys.includes(69) && kR.eNotes[0].y < this.y + 50 && kR.eNotes[0].y > this.y - 50 ){
+		kMain.score += 50 * multiplier;
+		kMain.rockLevel += 1;
+		kR.eNotes.shift();
+	}
+	else{
+		kMain.rockLevel -= 2;
+	}
+	
+	if(kMain.keys.includes(82) && kR.rNotes[0].y < this.y + 50 && kR.rNotes[0].y > this.y - 50 ){
+		kMain.score += 50 * multiplier;
+		kMain.rockLevel += 1;
+		kR.rNotes.shift();
+	}
+	else{
+		kMain.rockLevel -= 2;
+	}
+	
+	if(kMain.keys.includes(84) && kR.tNotes[0].y < this.y + 50 && kR.tNotes[0].y > this.y - 50 ){
+		kMain.score += 50 * multiplier;
+		kMain.rockLevel += 1;
+		kR.tNotes.shift();
+	}
+	else{
+		kMain.rockLevel -= 2;
+	}
+}
 
+/*
+	Q: 81
+	W: 87
+	E: 69
+	R: 82
+	T: 84
+*/
+// OnKeyDown
+document.onkeydown = function (e) {
+	//e = e || event; // to deal with IE
+    //map[e.keyCode] = e.type == 'keydown';
+	if(!kMain.keys.includes(e.keyCode)){
+		kMain.keys.push(e.keyCode);
+		//console.log(kMain.keys);
+		
+		if(e.keyCode == 32){
+			nReader.checkHit();
+		}
+	}
+	
+};
+
+//OnKeyUp
+document.onkeyup = function (e) {
+	var index = kMain.keys.indexOf(e.keyCode);
+	if (index !== -1) {
+        kMain.keys.splice(index, 1);
+    }
+	//console.log(kMain.keys);
 };
--- scripts/KeytarRender.js
+++ scripts/KeytarRender.js
@@ -12,11 +12,11 @@ function KeytarRender() {
   this.dogeY = 100;
   
   // Note rendering Queues 
-  var qNotes = [];
-  var wNotes = [];
-  var eNotes = [];
-  var rNotes = [];
-  var tNotes = [];
+  this.qNotes = [];
+  this.wNotes = [];
+  this.eNotes = [];
+  this.rNotes = [];
+  this.tNotes = [];
   
   //Initialization 
   var myInterval = setInterval(setCurrTime, 5);
@@ -26,7 +26,6 @@ function KeytarRender() {
 //GameSetUp
 KeytarRender.prototype.setupGame = function(){
 	this.drawBounds();
-	this.drawImage();
 };
 
 KeytarRender.prototype.clearCanvas = function() {
@@ -44,64 +43,80 @@ KeytarRender.prototype.drawBounds = function() {
 };
 
 //Draws Stuff on Canvas
-KeytarRender.prototype.drawImage = function() {
+function drawImage() {
 	
-	//Scope for onload
-	var that = this;
+	var that = kR;	
 	
-	var doge = new Image();
-
-	doge.onload = function() {
-		//console.log(that.ctx);
-		that.ctx.drawImage(doge, 0, that.dogeY, 100, 50);
-	};
+	that.clearCanvas();
 	
-	that.dogeY += 0.1;
-	doge.src = "images/doge.jpg"; 
-};
+		if(that.qNotes.length > 0)
+		{
+			for(var x = 0; x < that.qNotes.length; x++){
+				that.ctx.drawImage(that.qNotes[x].img, 0, that.qNotes[x].y, 100, 50);
+				that.qNotes[x].y += 5;
+			}
+		}
+	
+	//if(that.qNotes[0] != undefined)	
+	//	that.ctx.drawImage(that.qNotes[0].img, 0, that.qNotes[0].y, 100, 50);
+	
+	//console.log(this);
+	window.requestAnimationFrame(drawImage);
+}
 
 // setInterval function: Get's in game time 
 function setCurrTime(){
 	kR.currTime = Date.now() - kR.startTime;
 	//console.log(kR.currTime);
 	readOutNotes();
-	kR.drawImage();
-	kR.clearCanvas();
+	//kR.drawImage();
+	//kR.clearCanvas();
 }
 
 //Calls out Notes to spawn 
 function readOutNotes() {
 	
-	if(qArr.length > 0)
-	{
-		//console.log(kR.currTime + " " + kR.readRange);
-		
-		if(qArr[0] <= kR.currTime - kR.readRange)
+		//console.log(kR.qNotes);
+		if(kMain.qNotes.length > 0 && kMain.qNotes[0] <= kR.currTime - kR.readRange)
 		{
-			console.log("Q reader reading at time: " + qArr[0]);
-			qArr.shift();
+			console.log("Q reader reading at time: " + kMain.qNotes[0] + " " + kR.currTime);
+			//console.log(kR.qNotes);
+			kR.qNotes.push(new GreenNote());
+			kMain.qNotes.shift();
 		}
-		if(wArr[0] <= kR.currTime - kR.readRange)
+		if(kMain.wNotes.length > 0 && kMain.wNotes[0] <= kR.currTime - kR.readRange)
 		{
-			console.log("W reader reading at time: " + wArr[0]);
-			wArr.shift();
+			//console.log("W reader reading at time: " + kMain.wNotes[0]);
+			kR.wNotes.push(kMain.wNotes[0]);
+			kMain.wNotes.shift();
 		}
-		if(eArr[0] <= kR.currTime - kR.readRange)
+		if(kMain.eNotes.length > 0 && kMain.eNotes[0] <= kR.currTime - kR.readRange)
 		{
-			console.log("E reader reading at time: " + eArr[0]);
-			eArr.shift();
+			//console.log("E reader reading at time: " + kMain.eNotes[0]);
+			kR.eNotes.push(kMain.eNotes[0]);
+			kMain.eNotes.shift();
 		}
-		if(rArr[0] <= kR.currTime - kR.readRange)
+		if(kMain.rNotes.length > 0 && kMain.rNotes[0] <= kR.currTime - kR.readRange)
 		{
-			console.log("R reader reading at time: " + rArr[0]);
-			rArr.shift();
+			//console.log("R reader reading at time: " + kMain.rNotes[0]);
+			kR.rNotes.push(kMain.rNotes[0]);
+			kMain.rNotes.shift();
 		}
-		if(tArr[0] <= kR.currTime - kR.readRange)
+		if(kMain.tNotes.length > 0 && kMain.tNotes[0] <= kR.currTime - kR.readRange)
 		{
-			console.log("T reader reading at time: " + tArr[0]);
-			tArr.shift();
+			//console.log("T reader reading at time: " + kMain.tNotes[0]);
+			kR.tNotes.push(kMain.tNotes[0]);
+			kMain.tNotes.shift();
 		}
-	}
 }
 
+var kMain = new KeytarMain();
+kMain.loadSong(testSong);
+
+var nReader = new NoteReader();
+
 var kR = new KeytarRender();
+
+//console.log(kR.qNotes.length);
+
+window.requestAnimationFrame(drawImage);
